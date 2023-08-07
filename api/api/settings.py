d348aa78eac724f812f90c08be3d61673323900b
Mani Garg <72307020+manipta@users.noreply.github.com>
2023-08-07 07:09:51 +0530

    Update settings.py

--- api/api/settings.py
+++ api/api/settings.py
@@ -173,7 +173,8 @@ LOGGING = {
     },
 }
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static/"),
+    os.path.join(BASE_DIR, "static"),
 ]
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
