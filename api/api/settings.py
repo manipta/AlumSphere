ca838d2b7c2dee744c274dc2df78e246d107cedb
Mani Garg <72307020+manipta@users.noreply.github.com>
2023-08-07 07:15:32 +0530

    Update settings.py

--- api/api/settings.py
+++ api/api/settings.py
@@ -87,16 +87,16 @@ WSGI_APPLICATION = "api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresal',
-        'NAME': 'AlumniConnect',
-        'USER':'root',
-        'PASSWORD':'Vignesh@9909',
-        'HOST':'localhost',
-        'PORT':'3306',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresal',
+#         'NAME': 'AlumniConnect',
+#         'USER':'root',
+#         'PASSWORD':'Vignesh@9909',
+#         'HOST':'localhost',
+#         'PORT':'3306',
+#     }
+# }
 
 
 # Password validation
