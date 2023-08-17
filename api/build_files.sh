0f88599f8d95092cbefb379a68a05c1df87713c0
Mani Garg <manigarg2002@gmail.com>
2023-08-18 03:25:40 +0530

    deploy settings

--- api/build_files.sh
+++ api/build_files.sh
@@ -5,4 +5,5 @@ python3.9 manage.py makemigrations
 python3.9 manage.py migrate
 echo "Collect Static...."
 python3.9 manage.py collectstatic --noinput --clear
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@admin.com', 'anteater')" | python manage.py shell
 echo " BUILD END" 
