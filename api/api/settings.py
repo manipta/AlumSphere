1435b4dc573f64f01edb3b3024a09410a9f0b309
Mani Garg <manigarg2002@gmail.com>
2023-08-18 02:59:30 +0530

    deploy settings

--- api/api/settings.py
+++ api/api/settings.py
@@ -119,7 +119,7 @@ POSTGRES_READY = (
 if POSTGRES_READY:
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "ENGINE": "django.db.backends.postgresql",
             "NAME": POSTGRES_DB,
             "USER": POSTGRES_USER,
             "PASSWORD": POSTGRES_PASSWORD,
