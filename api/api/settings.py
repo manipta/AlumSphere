d979e10fae31f35306fbd6c17819c20e8dded1fe
Mani Garg <manigarg2002@gmail.com>
2023-08-18 02:55:58 +0530

    deploy settings

--- api/api/settings.py
+++ api/api/settings.py
@@ -16,7 +16,8 @@ from base import loggers
 import dj_database_url
 from decouple import config
 # from . import config
-
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
